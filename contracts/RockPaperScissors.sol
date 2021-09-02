06b83505600a865c20e9d7c4717ea8f01bc3c3a6
Javier <javier.1984@gmail.com>
2021-09-02 01:03:12 -0300

    added more unit tests

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -25,9 +25,12 @@ enum GameValue {
 
 error TransferFailed();
 error ShouldNeverHappen();
+//seconds in a week
+uint256 constant oneweek = 604800;
 
 contract RockPaperScissors is Ownable {
     IERC20 private erc20;
+    uint256 public time = 0;
 
     //gameid => player 1
     mapping(uint256 => address) public player1ForGame;
@@ -39,6 +42,8 @@ contract RockPaperScissors is Ownable {
     mapping(uint256 => uint256) public gameBet;
     //player addres => [gameId => gameValue]
     mapping(address => mapping(uint256 => GameValue)) private gamePlays;
+    //player 1 play timestamp
+    mapping(uint256 => uint256) private player1Timestamp;
     //player => balance
     mapping(address => uint256) public balances;
     //player => isPlaying ?
@@ -51,6 +56,8 @@ contract RockPaperScissors is Ownable {
     event Player1Wins(uint256 gameId);
     event Player2Wins(uint256 gameId);
     event Tie(uint256 gameId);
+    event Player1WithdrewBalance(uint256 gameId);
+    event Player2WithdrewBalance(uint256 gameId);
 
     constructor(IERC20 _erc20) {
         erc20 = _erc20;
@@ -79,10 +86,10 @@ contract RockPaperScissors is Ownable {
 
     function enrollPlayer2(uint256 _amount, uint256 gameId) public {
         //checks
+        require(activePlayer[_msgSender()] == false, "you can only play one game at once");
         require(status[gameId] == GameStatus.ENROLLING, "this game does not allow new players");
         require(player1ForGame[gameId] != address(0), "there is no player 1 for this game");
         require(gameBet[gameId] == _amount, "you have to match player 1 bet");
-        require(activePlayer[_msgSender()] == false, "you can only play one game at once");
 
         //effects
         player2ForGame[gameId] = _msgSender();
@@ -103,21 +110,22 @@ contract RockPaperScissors is Ownable {
         require(_msgSender() == player1ForGame[gameId], "you are not playing in this game");
 
         gamePlays[_msgSender()][gameId] = _val;
+        player1Timestamp[gameId] = getCurrentTime();
     }
 
     function player2Plays(GameValue _val, uint256 gameId) public onlyActiveGame(gameId) {
+        //checks
         require(_msgSender() == player2ForGame[gameId], "you are not playing in this game");
+        require(player1ForGame[gameId] != address(0), "you can't play, player 1 withdrew the bet");
+
+        //effects
         gamePlays[_msgSender()][gameId] = _val;
 
+        //interactions
         checkResultAndTransferPrize(gameId);
     }
 
-    modifier onlyActiveGame(uint256 gameId) {
-        require(status[gameId] == GameStatus.STARTED, "this game is not active");
-        _;
-    }
-
-    function checkResultAndTransferPrize(uint256 gameId) public {
+    function checkResultAndTransferPrize(uint256 gameId) private {
         address player1 = player1ForGame[gameId];
         address player2 = player2ForGame[gameId];
         GameValue value1 = gamePlays[player1][gameId];
@@ -128,10 +136,12 @@ contract RockPaperScissors is Ownable {
         }
         if (who == 1) {
             balances[player1] = balances[player1] + balances[player2];
+            balances[player2] = 0;
             emit Player1Wins(gameId);
         }
         if (who == 2) {
             balances[player2] = balances[player2] + balances[player1];
+            balances[player1] = 0;
             emit Player2Wins(gameId);
         }
         activePlayer[player1] = false;
@@ -139,18 +149,34 @@ contract RockPaperScissors is Ownable {
         status[gameId] = GameStatus.FINISHED;
     }
 
-    function withdrawBalance() public {
+    function player1WithdrawBalance(uint256 gameId) public {
+        require(player1ForGame[gameId] == _msgSender(), "invalid game id");
+        require((player1Timestamp[gameId] + oneweek) < getCurrentTime(), "you cannnot withdraw your bet yet");
+        require(status[gameId] == GameStatus.STARTED, "this game is not started");
+
         bool success = erc20.transfer(_msgSender(), balances[_msgSender()]);
         if (!success) {
             revert TransferFailed();
         }
+        activePlayer[_msgSender()] = false;
+        player1ForGame[gameId] = address(0);
+        balances[_msgSender()] = 0;
+        emit Player1WithdrewBalance(gameId);
     }
 
-    function checkMyBalance() public view returns (uint256) {
-        return balances[_msgSender()];
+    function player2WithdrawBalance(uint256 gameId) public {
+        require(player2ForGame[gameId] == _msgSender(), "invalid game id");
+        bool success = erc20.transfer(_msgSender(), balances[_msgSender()]);
+        if (!success) {
+            revert TransferFailed();
+        }
+        activePlayer[_msgSender()] = false;
+        player2ForGame[gameId] = address(0);
+        balances[_msgSender()] = 0;
+        emit Player2WithdrewBalance(gameId);
     }
 
-    function whoWins(GameValue _val1, GameValue _val2) public pure returns (uint8) {
+    function whoWins(GameValue _val1, GameValue _val2) private pure returns (uint8) {
         if (_val1 == GameValue.ROCK) {
             if (_val2 == GameValue.ROCK) {
                 return 0;
@@ -188,5 +214,25 @@ contract RockPaperScissors is Ownable {
         }
 
         revert ShouldNeverHappen();
+    }
+
+    //only for testing purposes, remove for prod
+    //this is used to mock current time for testing
+    function getCurrentTime() public view returns (uint256) {
+        if (time == 0) {
+            return block.timestamp;
+        } else {
+            return time;
+        }
+    }
+
+    //only for testing purposes, remove for prod
+    function setCurrentTime(uint256 _time) external {
+        time = _time;
+    }
+
+    modifier onlyActiveGame(uint256 gameId) {
+        require(status[gameId] == GameStatus.STARTED, "this game is not active");
+        _;
     }
 }
--- test/RockPaperScissors.ts
+++ test/RockPaperScissors.ts
@@ -1,9 +1,6 @@
-import hre from "hardhat";
-import { Artifact } from "hardhat/types";
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
-
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { expect } from "chai";
-import { ethers } from "hardhat";
+import hre from "hardhat";
 import { utils } from "ethers";
 import { MockDai, RockPaperScissors } from "../typechain";
 
@@ -13,28 +10,37 @@ enum GameStatus {
   FINISHED,
 }
 
+const ONEWEEK = 604800;
+const ONEDAY = 86400;
+
+enum GameValue {
+  ROCK,
+  PAPER,
+  SCISSORS
+}
+
 async function deployContracts(): Promise<[RockPaperScissors, MockDai]> {
-  var [owner, player1, player2] = await ethers.getSigners();
-  const DAI = await ethers.getContractFactory("MockDai");
+  var [owner, player1, player2] = await hre.ethers.getSigners();
+  const DAI = await await hre.ethers.getContractFactory("MockDai");
   const dai = await DAI.deploy();
   await dai.deployed();
 
   await dai.transfer(player1.address, utils.parseEther("3000"));
   await dai.transfer(player2.address, utils.parseEther("3000"));
 
-  const RockPaperScissors = await ethers.getContractFactory("RockPaperScissors", owner);
+  const RockPaperScissors = await hre.ethers.getContractFactory("RockPaperScissors", owner);
   const rockPaperScissors = await RockPaperScissors.deploy(dai.address);
 
   return [<RockPaperScissors>rockPaperScissors, <MockDai>dai];
 }
 
 describe("RockPaperScissors Unit tests", function () {
-  let owner: SignerWithAddress, player1: SignerWithAddress, player2: SignerWithAddress;
+  let owner, player1: SignerWithAddress, player2: SignerWithAddress;
 
-  before(async function () {});
+  before(async function () { });
 
   beforeEach(async function () {
-    [owner, player1, player2] = await ethers.getSigners();
+    [owner, player1, player2] = await hre.ethers.getSigners();
   });
 
   it("Players have enough money to play", async function () {
@@ -68,11 +74,318 @@ describe("RockPaperScissors Unit tests", function () {
     await expect((await dai.balanceOf(RPS.address)).toString()).to.equal(bet.toString());
   });
 
-  it("should enroll player 1 and 2", async function () {});
+  it("player 1 tries to enroll twice - should fail", async function () {
+    //arrange
+    var [RPS, dai] = await deployContracts();
+    let bet = utils.parseEther("1000");
 
-  it("should enroll player 1 with zero dai", async function () {});
+    let RPS1 = RPS.connect(player1);
+    let dai1 = dai.connect(player1);
 
-  it("player 2 tries to enroll to non existing game", async function () {});
+    await dai1.approve(RPS1.address, bet);
 
-  it("player 1 tries to enroll again after player 2 did", async function () {});
+    //act
+    await RPS1.enrollPlayer1(bet);
+    await expect(RPS1.enrollPlayer1(bet)).to.be.revertedWith("you can only play one game at once");
+  });
+
+  it("player 2 tries to enroll twice - should fail", async function () {
+
+    //arrange
+    var [RPS, dai] = await deployContracts();
+    let bet = utils.parseEther("1000");
+    let gameId = 1;
+
+    let RPS1 = RPS.connect(player1);
+    let RPS2 = RPS.connect(player2);
+
+    let dai1 = dai.connect(player1);
+    let dai2 = dai.connect(player2);
+
+    await dai1.approve(RPS1.address, bet);
+
+    //act
+    await expect(RPS1.enrollPlayer1(bet)).to.emit(RPS, "Player1Enrolled").withArgs(gameId);
+
+    await dai2.approve(RPS2.address, bet);
+
+    await expect(RPS2.enrollPlayer2(bet, gameId)).to.emit(RPS, "Player2Enrolled").withArgs(gameId);
+    await expect(RPS2.enrollPlayer2(bet, gameId)).to.be.revertedWith("you can only play one game at once");
+
+
+
+  });
+
+  it("should enroll player 1 and 2 - happy path", async function () {
+    //arrange
+    var [RPS, dai] = await deployContracts();
+    let bet = utils.parseEther("1000");
+    let gameId = 1;
+
+    let RPS1 = RPS.connect(player1);
+    let RPS2 = RPS.connect(player2);
+
+    let dai1 = dai.connect(player1);
+    let dai2 = dai.connect(player2);
+
+    await dai1.approve(RPS1.address, bet);
+
+    //act
+    await expect(RPS1.enrollPlayer1(bet)).to.emit(RPS, "Player1Enrolled").withArgs(gameId);
+
+    await dai2.approve(RPS2.address, bet);
+
+    await expect(RPS2.enrollPlayer2(bet, gameId)).to.emit(RPS, "Player2Enrolled").withArgs(gameId);
+
+  });
+
+  it("should enroll player 1 with zero dai", async function () {
+    //arrange
+    var [RPS, dai] = await deployContracts();
+    let bet = utils.parseEther("0");
+    let gameId = 1;
+
+    let RPS1 = RPS.connect(player1);
+    let dai1 = dai.connect(player1);
+
+    await dai1.approve(RPS1.address, bet);
+
+    //act
+    await expect(RPS1.enrollPlayer1(bet)).to.emit(RPS, "Player1Enrolled").withArgs(gameId);
+
+    const expectedBet = await RPS1.gameBet(gameId);
+    //assert
+    await expect((await RPS1.lastGameId()).toString()).to.equal(gameId + "");
+    await expect(expectedBet.toString()).to.equal(bet.toString());
+    await expect((await RPS1.player1ForGame(gameId)).toString()).to.equal(player1.address);
+    await expect((await RPS1.balances(player1.address)).toString()).to.equal(bet.toString());
+    await expect((await RPS1.status(gameId)).toString()).to.equal(GameStatus.ENROLLING + "");
+    await expect((await RPS1.activePlayer(player1.address)).toString()).to.equal("true");
+    await expect((await dai.balanceOf(RPS.address)).toString()).to.equal(bet.toString());
+
+  });
+
+  it("player 2 tries to enroll to non existing game - should fail", async function () {
+
+    //arrange
+    var [RPS, dai] = await deployContracts();
+    let bet = utils.parseEther("1000");
+    let gameId = 1;
+
+    let RPS1 = RPS.connect(player1);
+    let RPS2 = RPS.connect(player2);
+
+    let dai1 = dai.connect(player1);
+    let dai2 = dai.connect(player2);
+
+    await dai1.approve(RPS1.address, bet);
+
+    //act
+    await expect(RPS1.enrollPlayer1(bet)).to.emit(RPS, "Player1Enrolled").withArgs(gameId);
+
+    await dai2.approve(RPS2.address, bet);
+
+    const inexistentGame = 56
+
+    await expect(RPS2.enrollPlayer2(bet, inexistentGame)).to.be.revertedWith("there is no player 1 for this game");
+
+  });
+
+  it("player 1 tries to enroll again after player 2 did", async function () {
+
+
+    //arrange
+    var [RPS, dai] = await deployContracts();
+    let bet = utils.parseEther("1000");
+    let gameId = 1;
+
+    let RPS1 = RPS.connect(player1);
+    let RPS2 = RPS.connect(player2);
+
+    let dai1 = dai.connect(player1);
+    let dai2 = dai.connect(player2);
+
+    await dai1.approve(RPS1.address, bet);
+
+    //act
+    await expect(RPS1.enrollPlayer1(bet)).to.emit(RPS, "Player1Enrolled").withArgs(gameId);
+
+    await dai2.approve(RPS2.address, bet);
+
+    await expect(RPS2.enrollPlayer2(bet, gameId)).to.emit(RPS, "Player2Enrolled").withArgs(gameId);
+
+    await expect(RPS1.enrollPlayer1(bet)).to.be.revertedWith("you can only play one game at once");
+
+  });
+
+  it("player 1 plays, then player 2, it is a tie", async function () {
+    //arrange
+    var [RPS, dai] = await deployContracts();
+    let bet = utils.parseEther("1000");
+    let gameId = 1;
+
+    let RPS1 = RPS.connect(player1);
+    let RPS2 = RPS.connect(player2);
+
+    let dai1 = dai.connect(player1);
+    let dai2 = dai.connect(player2);
+
+    await dai1.approve(RPS1.address, bet);
+
+    //act
+    await expect(RPS1.enrollPlayer1(bet)).to.emit(RPS, "Player1Enrolled").withArgs(gameId);
+
+    await dai2.approve(RPS2.address, bet);
+
+    await expect(RPS2.enrollPlayer2(bet, gameId)).to.emit(RPS, "Player2Enrolled").withArgs(gameId);
+
+
+    //start playing
+
+    await RPS1.player1Plays(GameValue.PAPER, gameId);
+
+    await expect(await RPS2.player2Plays(GameValue.PAPER, gameId)).to.emit(RPS1, "Tie").withArgs(gameId);
+    await expect((await RPS2.balances(player1.address)).toString()).to.equal(utils.parseEther("1000").toString());
+    await expect((await RPS2.balances(player2.address)).toString()).to.equal(utils.parseEther("1000").toString());
+
+  });
+
+  it("player 1 plays, then player 2, player 1 wins", async function () {
+
+    //arrange
+    var [RPS, dai] = await deployContracts();
+    let bet = utils.parseEther("1000");
+    let gameId = 1;
+
+    let RPS1 = RPS.connect(player1);
+    let RPS2 = RPS.connect(player2);
+
+    let dai1 = dai.connect(player1);
+    let dai2 = dai.connect(player2);
+
+    await dai1.approve(RPS1.address, bet);
+
+    //act
+    await expect(RPS1.enrollPlayer1(bet)).to.emit(RPS, "Player1Enrolled").withArgs(gameId);
+
+    await dai2.approve(RPS2.address, bet);
+
+    await expect(RPS2.enrollPlayer2(bet, gameId)).to.emit(RPS, "Player2Enrolled").withArgs(gameId);
+
+
+    //start playing
+
+    await RPS1.player1Plays(GameValue.PAPER, gameId);
+
+    await expect(await RPS2.player2Plays(GameValue.ROCK, gameId)).to.emit(RPS1, "Player1Wins").withArgs(gameId);
+
+    await expect((await RPS2.balances(player1.address)).toString()).to.equal(utils.parseEther("2000").toString());
+    await expect((await RPS2.balances(player2.address)).toString()).to.equal(utils.parseEther("0").toString());
+  });
+
+  it("player 1 plays, then player 2, player 2 wins", async function () {
+    //arrange
+    var [RPS, dai] = await deployContracts();
+    let bet = utils.parseEther("1000");
+    let gameId = 1;
+
+    let RPS1 = RPS.connect(player1);
+    let RPS2 = RPS.connect(player2);
+
+    let dai1 = dai.connect(player1);
+    let dai2 = dai.connect(player2);
+
+    await dai1.approve(RPS1.address, bet);
+
+    //act
+    await expect(RPS1.enrollPlayer1(bet)).to.emit(RPS, "Player1Enrolled").withArgs(gameId);
+
+    await dai2.approve(RPS2.address, bet);
+
+    await expect(RPS2.enrollPlayer2(bet, gameId)).to.emit(RPS, "Player2Enrolled").withArgs(gameId);
+
+
+    //start playing
+
+    await RPS1.player1Plays(GameValue.ROCK, gameId);
+
+    await expect(await RPS2.player2Plays(GameValue.PAPER, gameId)).to.emit(RPS1, "Player2Wins").withArgs(gameId);
+
+    await expect((await RPS2.balances(player1.address)).toString()).to.equal(utils.parseEther("0").toString());
+    await expect((await RPS2.balances(player2.address)).toString()).to.equal(utils.parseEther("2000").toString());
+  });
+
+  it("player 1 plays and tries to withdraw bet in less than a week", async function () {
+    //arrange
+    var [RPS, dai] = await deployContracts();
+    let bet = utils.parseEther("1000");
+    let gameId = 1;
+
+    let RPS1 = RPS.connect(player1);
+    let RPS2 = RPS.connect(player2);
+
+    let dai1 = dai.connect(player1);
+    let dai2 = dai.connect(player2);
+
+    await dai1.approve(RPS1.address, bet);
+    await RPS1.enrollPlayer1(bet);
+
+    await dai2.approve(RPS2.address, bet);
+    await RPS2.enrollPlayer2(bet, gameId);
+
+    await RPS1.player1Plays(GameValue.ROCK, gameId);
+
+    IncreaseTime(RPS1, ONEDAY);
+
+    await expect(RPS1.player1WithdrawBalance(gameId)).to.be.revertedWith("you cannnot withdraw your bet yet");
+
+  });
+
+
+  it("player 1 plays,  player 2 does not play after one week, then player 1 recovers funds", async function () {
+    //arrange
+    var [RPS, dai] = await deployContracts();
+    let bet = utils.parseEther("1000");
+    let gameId = 1;
+
+    let RPS1 = RPS.connect(player1);
+    let RPS2 = RPS.connect(player2);
+
+    let dai1 = dai.connect(player1);
+    let dai2 = dai.connect(player2);
+
+    await dai1.approve(RPS1.address, bet);
+
+    //act
+    await expect(RPS1.enrollPlayer1(bet)).to.emit(RPS, "Player1Enrolled").withArgs(gameId);
+
+    await dai2.approve(RPS2.address, bet);
+
+    await expect(RPS2.enrollPlayer2(bet, gameId)).to.emit(RPS, "Player2Enrolled").withArgs(gameId);
+
+    await RPS1.player1Plays(GameValue.ROCK, gameId);
+
+    await IncreaseTime(RPS1, ONEWEEK * 2);
+
+    await expect(RPS1.player1WithdrawBalance(gameId)).to.emit(RPS1, "Player1WithdrewBalance").withArgs(gameId);
+    await expect((await RPS1.activePlayer(player1.address)).toString()).to.equal("false")
+    await expect((await RPS1.player1ForGame(gameId)).toString()).to.equal("0x0000000000000000000000000000000000000000");
+    await expect((await RPS1.balances(player1.address)).toString()).to.equal("0");
+    await expect((await dai1.balanceOf(player1.address))).to.equal(utils.parseEther("3000"));
+
+
+  });
+
+
+  it("player 1 plays withdrew bet after a week, then player 2 does the same after one week", async function () {
+
+
+  });
+
+  async function IncreaseTime(contract: RockPaperScissors, seconds: number) {
+    let time = await contract.getCurrentTime();
+    time = time.add(seconds);
+    await contract.setCurrentTime(time);
+  }
+
 });
