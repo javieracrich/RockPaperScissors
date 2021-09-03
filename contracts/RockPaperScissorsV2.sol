c84f73e14df26e130dc4ea0a4a31b012551c7433
Javier <javier.1984@gmail.com>
2021-09-03 17:53:13 -0300

    version 2 with commit reveal pattern

---
+++ contracts/RockPaperScissorsV2.sol
@@ -0,0 +1,302 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+
+enum GameStatus {
+    COMMITING,
+    REVEALING,
+    FINISHED
+}
+
+enum Choice {
+    ROCK,
+    PAPER,
+    SCISSORS
+}
+
+error TransferFailed();
+error ShouldNeverHappen();
+//seconds in a week
+uint256 constant oneweek = 604800;
+
+struct CommitChoice {
+    address player;
+    Choice choice;
+    uint256 timestamp;
+}
+
+contract RockPaperScissorsV2 is Context {
+    IERC20 private erc20;
+    uint256 public time = 0;
+    uint256 public lastGameId = 0;
+
+    //player => password
+    mapping(address => bytes32) private playerPasswords;
+    // //gameid => player 1
+    mapping(uint256 => address) public player1ForGame;
+    // //gameid => player 2
+    mapping(uint256 => address) public player2ForGame;
+
+    mapping(uint256 => CommitChoice) public commitChoicesForGame;
+
+    //gameid => game status
+    mapping(uint256 => GameStatus) public status;
+    //gameid => player 1 bet
+    mapping(uint256 => uint256) public gameBet;
+    //player addres => [gameId => bytes32 hash]
+    mapping(address => mapping(uint256 => bytes32)) public commits;
+
+    //gameiD=> player1 choice
+    mapping(uint256 => Choice) public player1Choices;
+
+    //gameId => player1 revealead
+    mapping(uint256 => bool) public player1Revealed;
+
+    //player 1 play timestamp
+    mapping(uint256 => uint256) public player1Timestamp;
+
+    //gameId player 2 revealed
+    mapping(uint256 => bool) public player2Revealed;
+
+    //gameid => player 2 choice
+    mapping(uint256 => Choice) public player2Choices;
+
+    //player => balance
+    mapping(address => uint256) public balances;
+
+    event Player1Commited(uint256 gameId);
+    event Player2Commited(uint256 gameId);
+    event Player1Wins(uint256 gameId);
+    event Player2Wins(uint256 gameId);
+    event Tie(uint256 gameId);
+    event Player1WithdrewBalance(uint256 gameId);
+    event Player2WithdrewBalance(uint256 gameId);
+    event Player1Revealed(uint256 gameId);
+    event Player2Revealed(uint256 gameId);
+
+    constructor(IERC20 _erc20) {
+        erc20 = _erc20;
+    }
+
+    function login(string memory password) public {
+        require(playerPasswords[_msgSender()] == bytes32(0), "you have already logged in");
+        playerPasswords[_msgSender()] = hashPassword(password);
+    }
+
+    function hashChoice(Choice data) public view returns (bytes32) {
+        return keccak256(abi.encodePacked(address(this), playerPasswords[_msgSender()], data));
+    }
+
+    function hashPassword(string memory password) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(password));
+    }
+
+    modifier onlyLoggedIn(string memory password) {
+        require(playerPasswords[_msgSender()] != bytes32(0), "you are not loggedIn");
+        require(playerPasswords[_msgSender()] == hashPassword(password), "invalid password");
+        _;
+    }
+
+    function player1Commit(
+        Choice choice,
+        uint256 _amount,
+        string memory password
+    ) external onlyLoggedIn(password) {
+        // effects
+        lastGameId++;
+
+        gameBet[lastGameId] = _amount;
+
+        player1ForGame[lastGameId] = _msgSender();
+
+        status[lastGameId] = GameStatus.COMMITING;
+
+        player1Timestamp[lastGameId] = getCurrentTime();
+
+        balances[_msgSender()] = _amount;
+
+        commits[_msgSender()][lastGameId] = hashChoice(choice);
+
+        //interactions
+        bool success = erc20.transferFrom(_msgSender(), address(this), _amount);
+        if (!success) {
+            revert TransferFailed();
+        }
+
+        emit Player1Commited(lastGameId);
+    }
+
+    //only for testing purposes, remove for prod
+    //this is used to mock current time for testing
+    function getCurrentTime() public view returns (uint256) {
+        if (time == 0) {
+            return block.timestamp;
+        } else {
+            return time;
+        }
+    }
+
+    //only for testing purposes, remove for prod
+    function setCurrentTime(uint256 _time) external {
+        time = _time;
+    }
+
+    function player2Commit(
+        Choice choice,
+        uint256 _amount,
+        uint256 gameId,
+        string memory password
+    ) external onlyLoggedIn(password) {
+        //checks
+        require(status[gameId] == GameStatus.COMMITING, "this game is not the COMMITING phase");
+        require(player1ForGame[gameId] != address(0), "there is no player 1 for this game");
+        require(gameBet[gameId] == _amount, "you have to match player 1 bet");
+        require(player1ForGame[gameId] != address(0), "you can't play because player 1 withdrew the bet");
+
+        //effects
+        player2ForGame[gameId] = _msgSender();
+        status[gameId] = GameStatus.REVEALING;
+        balances[_msgSender()] = _amount;
+        commits[_msgSender()][lastGameId] = hashChoice(choice);
+
+        //interactions
+        bool success = erc20.transferFrom(_msgSender(), address(this), _amount);
+        if (!success) {
+            revert TransferFailed();
+        }
+
+        emit Player2Commited(gameId);
+    }
+
+    function player1Reveals(
+        Choice choice,
+        uint256 gameId,
+        string memory password
+    ) external onlyLoggedIn(password) {
+        require(status[gameId] == GameStatus.REVEALING, "this game is not the REVEALING phase");
+        require(player1ForGame[gameId] == _msgSender(), "you are not playing this game");
+
+        bool valid = hashChoice(choice) == commits[_msgSender()][gameId];
+        require(valid, "invalid choice hash");
+
+        player1Choices[gameId] = choice;
+        player1Revealed[gameId] = true;
+
+        emit Player1Revealed(gameId);
+    }
+
+    function player2Reveals(
+        Choice choice,
+        uint256 gameId,
+        string memory password
+    ) external onlyLoggedIn(password) {
+        require(player1Revealed[gameId] == true, "player 1 has to reveal first");
+
+        require(status[gameId] == GameStatus.REVEALING, "this game is not the REVEALING phase");
+        require(player2ForGame[gameId] == _msgSender(), "you are not playing this game");
+
+        bool valid = hashChoice(choice) == commits[_msgSender()][gameId];
+        require(valid, "invalid choice hash");
+
+        player2Choices[gameId] = choice;
+        player2Revealed[gameId] = true;
+        emit Player2Revealed(gameId);
+    }
+
+    function distribute(uint256 gameId) external {
+        require(status[gameId] != GameStatus.FINISHED, "this game has already been distributed");
+        require(player1Revealed[gameId] == true, "player 1 has to reveal");
+        require(player2Revealed[gameId] == true, "player 2 has to reveal");
+
+        address player1 = player1ForGame[gameId];
+        address player2 = player2ForGame[gameId];
+
+        uint8 who = whoWins(player1Choices[gameId], player2Choices[gameId]);
+        if (who == 0) {
+            emit Tie(gameId);
+        }
+        if (who == 1) {
+            balances[player1] = balances[player1] + balances[player2];
+            balances[player2] = 0;
+            emit Player1Wins(gameId);
+        }
+        if (who == 2) {
+            balances[player2] = balances[player2] + balances[player1];
+            balances[player1] = 0;
+            emit Player2Wins(gameId);
+        }
+        status[gameId] = GameStatus.FINISHED;
+
+        bool success1 = erc20.transfer(player1, balances[player1]);
+        if (!success1) {
+            revert TransferFailed();
+        }
+
+        bool success2 = erc20.transfer(player2, balances[player2]);
+        if (!success2) {
+            revert TransferFailed();
+        }
+    }
+
+    function player1WithdrawBalance(uint256 gameId) external {
+        require(player1ForGame[gameId] == _msgSender(), "invalid game id");
+        require((player1Timestamp[gameId] + 1 weeks) < getCurrentTime(), "you cannnot withdraw your bet yet");
+        require(status[gameId] == GameStatus.COMMITING, "this game is not started");
+
+        bool success = erc20.transfer(_msgSender(), gameBet[gameId]);
+        if (!success) {
+            revert TransferFailed();
+        }
+        player1ForGame[gameId] = address(0);
+        balances[_msgSender()] = 0;
+
+        emit Player1WithdrewBalance(gameId);
+    }
+
+    function player2WithdrawBalance(uint256 gameId) external {
+        require(player2ForGame[gameId] == _msgSender(), "invalid game id");
+
+        bool success = erc20.transfer(_msgSender(), gameBet[gameId]);
+        if (!success) {
+            revert TransferFailed();
+        }
+        player2ForGame[gameId] = address(0);
+        balances[_msgSender()] = 0;
+
+        emit Player2WithdrewBalance(gameId);
+    }
+
+    function whoWins(Choice choice1, Choice choice2) private pure returns (uint8) {
+        if (choice1 == choice2) {
+            return 0;
+        }
+        if (choice1 == Choice.ROCK) {
+            if (choice2 == Choice.PAPER) {
+                return 2;
+            }
+            if (choice2 == Choice.SCISSORS) {
+                return 1;
+            }
+        }
+        if (choice1 == Choice.PAPER) {
+            if (choice2 == Choice.ROCK) {
+                return 1;
+            }
+            if (choice2 == Choice.SCISSORS) {
+                return 2;
+            }
+        }
+        if (choice1 == Choice.SCISSORS) {
+            if (choice2 == Choice.ROCK) {
+                return 2;
+            }
+            if (choice2 == Choice.PAPER) {
+                return 1;
+            }
+        }
+        revert ShouldNeverHappen();
+    }
+}
---
+++ test/RockPaperScissors2.ts
@@ -0,0 +1,398 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { expect } from "chai";
+import hre from "hardhat";
+import { utils } from "ethers";
+import { MockDai } from "../typechain";
+import { RockPaperScissorsV2 } from '../typechain/RockPaperScissorsV2';
+
+enum GameStatus {
+  COMMITING,
+  REVEALING,
+  FINISHED
+}
+
+enum Choice {
+  ROCK = 0,
+  PAPER = 1,
+  SCISSORS = 2
+}
+
+enum GamePlay {
+  ROCK = 1,
+  PAPER = 2,
+  SCISSORS = 3
+}
+
+const ONEWEEK = 604800;
+const ONEDAY = 86400;
+
+async function deployContracts(): Promise<[RockPaperScissorsV2, MockDai]> {
+  const [owner, player1, player2] = await hre.ethers.getSigners();
+  const DAI = await await hre.ethers.getContractFactory("MockDai");
+  const dai = await DAI.deploy();
+  await dai.deployed();
+
+  await dai.transfer(player1.address, utils.parseEther("3000"));
+  await dai.transfer(player2.address, utils.parseEther("3000"));
+
+  const RockPaperScissors = await hre.ethers.getContractFactory("RockPaperScissorsV2", owner);
+  const rockPaperScissors = await RockPaperScissors.deploy(dai.address);
+
+  return [<RockPaperScissorsV2>rockPaperScissors, <MockDai>dai];
+}
+
+describe("RockPaperScissorsv2 Unit tests", function () {
+  let owner, player1: SignerWithAddress, player2: SignerWithAddress;
+
+  beforeEach(async function () {
+    [owner, player1, player2] = await hre.ethers.getSigners();
+
+
+  });
+
+  it("Players have enough money to play", async function () {
+    const [_, dai] = await deployContracts();
+    expect(await dai.balanceOf(player1.address)).to.equal(utils.parseEther("3000"));
+    expect(await dai.balanceOf(player1.address)).to.equal(utils.parseEther("3000"));
+  });
+
+  it("Player 1 commits choice with 1000 dai", async function () {
+    //arrange
+    const [RPS, dai] = await deployContracts();
+    const bet = utils.parseEther("1000");
+    const gameId = 1;
+
+    const RPS1 = RPS.connect(player1);
+    const dai1 = dai.connect(player1);
+
+    await dai1.approve(RPS1.address, bet);
+
+    const password = "P@assword123!";
+    await RPS1.login(password);
+
+    //act
+    await expect(RPS1.player1Commit(Choice.ROCK, bet, password)).to.emit(RPS, "Player1Commited").withArgs(gameId);
+
+    //assert
+    await expect((await RPS1.lastGameId()).toString()).to.equal(gameId + "");
+    await expect((await RPS1.gameBet(gameId)).toString()).to.equal(bet.toString());
+    await expect((await RPS1.player1ForGame(gameId)).toString()).to.equal(player1.address);
+    await expect((await RPS1.status(gameId)).toString()).to.equal(GameStatus.COMMITING + "");
+    await expect((await RPS1.player1Timestamp(gameId)).toString()).to.not.equal("0");
+    await expect((await RPS1.balances(player1.address)).toString()).to.equal(bet.toString());
+    await expect((await RPS1.commits(player1.address, gameId)).toString()).to.equal("0xc3281f7ded705e5487c6d21ef2da15bb73847ec322e9a3d39a5482c154e5f3d6");
+    await expect((await dai.balanceOf(RPS.address)).toString()).to.equal(bet.toString());
+  });
+
+  it("player 1 and 2 commit with 1000 dai, then they both reveal, it is a tie, player 1 executes prize distribution, there is no balance to distribute", async function () {
+    //arrange
+    const [RPS, dai] = await deployContracts();
+    const bet = utils.parseEther("1000");
+    const gameId = 1;
+
+    const RPS1 = RPS.connect(player1);
+    const dai1 = dai.connect(player1);
+
+    const RPS2 = RPS.connect(player2);
+    const dai2 = dai.connect(player2);
+
+    await dai1.approve(RPS1.address, bet);
+    await dai2.approve(RPS2.address, bet);
+
+    const player1Password = "P@assword123!";
+    const player2Password = "P@assword456!Secret";
+    await RPS1.login(player1Password);
+
+    await RPS2.login(player2Password);
+
+    //act
+    await expect(RPS1.player1Commit(Choice.ROCK, bet, player1Password)).to.emit(RPS, "Player1Commited").withArgs(gameId);
+
+    await expect(RPS2.player2Commit(Choice.ROCK, bet, gameId, player2Password)).to.emit(RPS, "Player2Commited").withArgs(gameId);
+
+    await expect((await RPS.player1Revealed(player1.address)).toString()).to.equal(false.toString());
+
+    await expect((await RPS.player2Revealed(player1.address)).toString()).to.equal(false.toString());
+
+    await expect(RPS1.player1Reveals(Choice.ROCK, gameId, player1Password)).to.emit(RPS, "Player1Revealed").withArgs(gameId);
+
+    await expect((await RPS1.player1Choices(gameId)).toString()).to.equal(Choice.ROCK.toString());
+    await expect((await RPS1.player1Revealed(gameId)).toString()).to.equal(true.toString());
+
+    await expect(RPS2.player2Reveals(Choice.ROCK, gameId, player2Password)).to.emit(RPS, "Player2Revealed").withArgs(gameId);
+
+    await expect((await RPS2.player2Choices(gameId)).toString()).to.equal(Choice.ROCK.toString());
+    await expect((await RPS1.player2Revealed(gameId)).toString()).to.equal(true.toString());
+
+    await expect(RPS1.distribute(gameId)).to.emit(RPS, "Tie");
+
+    await expect(await RPS1.balances(player1.address)).to.equal(utils.parseEther("1000"))
+    await expect(await RPS2.balances(player2.address)).to.equal(utils.parseEther("1000"))
+
+    await expect(await dai.balanceOf(player1.address)).to.equal(utils.parseEther("3000"))
+    await expect(await dai.balanceOf(player2.address)).to.equal(utils.parseEther("3000"))
+
+    expect(await RPS.status(gameId)).to.equal(GameStatus.FINISHED);
+
+
+  });
+
+  it("player 2 wants to distribute without having revealed commits", async function () {
+    //arrange
+    const [RPS, dai] = await deployContracts();
+    const bet = utils.parseEther("1000");
+    const gameId = 1;
+
+    const RPS1 = RPS.connect(player1);
+    const dai1 = dai.connect(player1);
+
+    const RPS2 = RPS.connect(player2);
+    const dai2 = dai.connect(player2);
+
+    await dai1.approve(RPS1.address, bet);
+    await dai2.approve(RPS2.address, bet);
+
+    const player1Password = "P@assword123!";
+    const player2Password = "P@assword456!Secret";
+    await RPS1.login(player1Password);
+
+    await RPS2.login(player2Password);
+
+    //act
+    await expect(RPS1.player1Commit(Choice.ROCK, bet, player1Password)).to.emit(RPS, "Player1Commited").withArgs(gameId);
+
+    await expect(RPS2.player2Commit(Choice.ROCK, bet, gameId, player2Password)).to.emit(RPS, "Player2Commited").withArgs(gameId);
+
+    await expect(RPS2.distribute(gameId)).to.be.revertedWith("player 1 has to reveal");
+
+  });
+
+
+  it("player 1 and 2 commit with 1000 dai, then they both reveal, player 1 wins, player 1 executes prize distribution", async function () {
+    const [RPS, dai] = await deployContracts();
+    const bet = utils.parseEther("1000");
+    const gameId = 1;
+
+    const RPS1 = RPS.connect(player1);
+    const dai1 = dai.connect(player1);
+
+    const RPS2 = RPS.connect(player2);
+    const dai2 = dai.connect(player2);
+
+    await dai1.approve(RPS1.address, bet);
+    await dai2.approve(RPS2.address, bet);
+
+    const player1Password = "P@assword123!";
+    const player2Password = "P@assword456!Secret";
+    await RPS1.login(player1Password);
+
+    await RPS2.login(player2Password);
+
+    //act
+    await expect(RPS1.player1Commit(Choice.PAPER, bet, player1Password)).to.emit(RPS, "Player1Commited").withArgs(gameId);
+
+    await expect(RPS2.player2Commit(Choice.ROCK, bet, gameId, player2Password)).to.emit(RPS, "Player2Commited").withArgs(gameId);
+
+    await expect((await RPS.player1Revealed(player1.address)).toString()).to.equal(false.toString());
+
+    await expect((await RPS.player2Revealed(player1.address)).toString()).to.equal(false.toString());
+
+    await expect(RPS1.player1Reveals(Choice.PAPER, gameId, player1Password)).to.emit(RPS, "Player1Revealed").withArgs(gameId);
+
+    await expect((await RPS1.player1Choices(gameId)).toString()).to.equal(Choice.PAPER.toString());
+    await expect((await RPS1.player1Revealed(gameId)).toString()).to.equal(true.toString());
+
+    await expect(RPS2.player2Reveals(Choice.ROCK, gameId, player2Password)).to.emit(RPS, "Player2Revealed").withArgs(gameId);
+
+    await expect((await RPS2.player2Choices(gameId)).toString()).to.equal(Choice.ROCK.toString());
+    await expect((await RPS1.player2Revealed(gameId)).toString()).to.equal(true.toString());
+
+    await expect(RPS1.distribute(gameId)).to.emit(RPS, "Player1Wins").withArgs(gameId);
+
+    await expect(await RPS1.balances(player1.address)).to.equal(utils.parseEther("2000"))
+    await expect(await RPS2.balances(player2.address)).to.equal(utils.parseEther("0"))
+
+    await expect(await dai.balanceOf(player1.address)).to.equal(utils.parseEther("4000"))
+    await expect(await dai.balanceOf(player2.address)).to.equal(utils.parseEther("2000"))
+
+    expect(await RPS.status(gameId)).to.equal(GameStatus.FINISHED);
+
+
+
+
+  });
+
+  it("player 1 and 2 commit with 1000 dai, then they both reveal, player 2 wins, player 2 executes prize distribution", async function () {
+    const [RPS, dai] = await deployContracts();
+    const bet = utils.parseEther("1000");
+    const gameId = 1;
+
+    const RPS1 = RPS.connect(player1);
+    const dai1 = dai.connect(player1);
+
+    const RPS2 = RPS.connect(player2);
+    const dai2 = dai.connect(player2);
+
+    await dai1.approve(RPS1.address, bet);
+    await dai2.approve(RPS2.address, bet);
+
+    const player1Password = "P@assword123!";
+    const player2Password = "P@assword456!Secret";
+    await RPS1.login(player1Password);
+
+    await RPS2.login(player2Password);
+
+    //act
+    await expect(RPS1.player1Commit(Choice.PAPER, bet, player1Password)).to.emit(RPS, "Player1Commited").withArgs(gameId);
+
+    await expect(RPS2.player2Commit(Choice.SCISSORS, bet, gameId, player2Password)).to.emit(RPS, "Player2Commited").withArgs(gameId);
+
+    await expect((await RPS.player1Revealed(player1.address)).toString()).to.equal(false.toString());
+
+    await expect((await RPS.player2Revealed(player1.address)).toString()).to.equal(false.toString());
+
+    await expect(RPS1.player1Reveals(Choice.PAPER, gameId, player1Password)).to.emit(RPS, "Player1Revealed").withArgs(gameId);
+
+    await expect((await RPS1.player1Choices(gameId)).toString()).to.equal(Choice.PAPER.toString());
+    await expect((await RPS1.player1Revealed(gameId)).toString()).to.equal(true.toString());
+
+    await expect(RPS2.player2Reveals(Choice.SCISSORS, gameId, player2Password)).to.emit(RPS, "Player2Revealed").withArgs(gameId);
+
+    await expect((await RPS2.player2Choices(gameId)).toString()).to.equal(Choice.SCISSORS.toString());
+    await expect((await RPS2.player2Revealed(gameId)).toString()).to.equal(true.toString());
+
+    await expect(RPS2.distribute(gameId)).to.emit(RPS, "Player2Wins").withArgs(gameId);
+
+    await expect(await RPS1.balances(player1.address)).to.equal(utils.parseEther("0"))
+    await expect(await RPS2.balances(player2.address)).to.equal(utils.parseEther("2000"))
+
+    await expect(await dai.balanceOf(player1.address)).to.equal(utils.parseEther("2000"))
+    await expect(await dai.balanceOf(player2.address)).to.equal(utils.parseEther("4000"))
+
+    expect(await RPS.status(gameId)).to.equal(GameStatus.FINISHED);
+
+
+
+
+  });
+
+
+
+  it("player 1 commits, player 2 never commits after a week, player 1 withraws bet, player 2 does the same", async function () {
+    const [RPS, dai] = await deployContracts();
+    const bet = utils.parseEther("1000");
+    const gameId = 1;
+
+    const RPS1 = RPS.connect(player1);
+    const dai1 = dai.connect(player1);
+
+    const RPS2 = RPS.connect(player2);
+    const dai2 = dai.connect(player2);
+
+    await dai1.approve(RPS1.address, bet);
+    await dai2.approve(RPS2.address, bet);
+
+    const player1Password = "P@assword123!";
+    const player2Password = "P@assword456!Secret";
+    await RPS1.login(player1Password);
+
+    await RPS2.login(player2Password);
+
+    //act
+    await expect(RPS1.player1Commit(Choice.PAPER, bet, player1Password)).to.emit(RPS, "Player1Commited").withArgs(gameId);
+
+    await IncreaseTime(RPS, ONEWEEK * 2);
+
+    await expect(RPS1.player1WithdrawBalance(gameId)).to.emit(RPS, "Player1WithdrewBalance").withArgs(gameId);
+
+    await expect((await RPS.player1ForGame(gameId)).toString()).to.equal("0x0000000000000000000000000000000000000000");
+    await expect((await RPS.balances(player1.address)).toString()).to.equal("0");
+
+
+
+
+
+  });
+
+  it("player 2 wants to play after player 1 withdrew the bet after 1 week", async function () {
+    const [RPS, dai] = await deployContracts();
+    const bet = utils.parseEther("1000");
+    const gameId = 1;
+
+    const RPS1 = RPS.connect(player1);
+    const dai1 = dai.connect(player1);
+
+    const RPS2 = RPS.connect(player2);
+    const dai2 = dai.connect(player2);
+
+    await dai1.approve(RPS1.address, bet);
+    await dai2.approve(RPS2.address, bet);
+
+    const player1Password = "P@assword123!";
+    const player2Password = "P@assword456!Secret";
+    await RPS1.login(player1Password);
+
+    await RPS2.login(player2Password);
+
+    //act
+    await expect(RPS1.player1Commit(Choice.PAPER, bet, player1Password)).to.emit(RPS, "Player1Commited").withArgs(gameId);
+
+    await IncreaseTime(RPS, ONEWEEK * 2);
+
+    await expect(RPS1.player1WithdrawBalance(gameId)).to.emit(RPS, "Player1WithdrewBalance").withArgs(gameId);
+
+    await expect(RPS2.player2Commit(Choice.PAPER, bet, gameId, player2Password)).to.be.revertedWith("there is no player 1 for this game");
+
+
+  });
+
+
+
+  it("player 2 tries to play in a game with no player 1", async function () {
+
+    const [RPS, dai] = await deployContracts();
+    const bet = utils.parseEther("1000");
+    const gameId = 1;
+
+    const RPS1 = RPS.connect(player1);
+    const dai1 = dai.connect(player1);
+
+    const RPS2 = RPS.connect(player2);
+    const dai2 = dai.connect(player2);
+
+    await dai1.approve(RPS1.address, bet);
+    await dai2.approve(RPS2.address, bet);
+
+    const player1Password = "P@assword123!";
+    const player2Password = "P@assword456!Secret";
+    await RPS1.login(player1Password);
+
+    await RPS2.login(player2Password);
+
+    //act
+
+    await expect(RPS2.player2Commit(Choice.PAPER, bet, gameId, player2Password)).to.be.revertedWith("there is no player 1 for this game");
+
+  });
+
+  it("player 2 bet does not match player 1 bet", async function () {
+
+  });
+
+
+  it("player 1 commits and tries to withdraw bet in afer 1 day", async function () {
+
+
+  });
+
+  it("player 1 commits in different games, and then player 2 plays. ", async function () {
+
+
+  });
+
+  async function IncreaseTime(contract: RockPaperScissorsV2, seconds: number) {
+    let time = await contract.getCurrentTime();
+    time = time.add(seconds);
+    await contract.setCurrentTime(time);
+  }
+});
